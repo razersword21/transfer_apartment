7da4b3f7a001d689fe4486ab6b11e645c44bbdfe
unknown <razer10409@gmail.com>
2024-10-20 14:32:07 +0800

    feat: 修改測試

---
+++ update_mamory_stream.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
+
+model_name = "Qwen/Qwen2.5-3B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
