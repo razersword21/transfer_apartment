c79d71cd41a60050a8edb09c1efdbcf902ca0fb6
unknown <razer10409@gmail.com>
2024-10-21 17:58:03 +0800

    feat: 增加檢查生成結果機制和寫入人物檔

--- daily_routine.py
+++ daily_routine.py
@@ -1,11 +1,22 @@
 from qwen_3b_llm import *
 from prompt import *
+from process_method import *
 import json
 
-with open("personal_information\lin\information.json", "r", encoding="utf-8") as f:
+
+with open("personal_information\wang\information.json", "r", encoding="utf-8") as f:
     person_information = json.load(f)
 
-daily_schedule, times = make_design(person_information, daily_routine)
+check_json_format_flag = False
 
-print(daily_schedule)
+while(check_json_format_flag == False):
+    daily_schedule, times = make_design(person_information['background'], daily_routine)
+    print("生成結果: {}".format(daily_schedule))
+    daily_schedule, check_json_format_flag = check_json_format(daily_schedule, check_json_format_flag)
+    print("檢查狀態: {}".format(check_json_format_flag))
+
+person_information['schedule'] = daily_schedule['today_schedule']
+with open("personal_information\wang\information.json", "w", encoding="utf-8") as f:
+    json.dump(person_information, f, ensure_ascii=False, indent=1)
+
 print("執行時間:", times)
--- design_action.py
+++ design_action.py
@@ -4,7 +4,7 @@ import json
 
 with open("personal_information\lin\information.json", "r", encoding="utf-8") as f:
     person_information = json.load(f)
-
+design_action.format(action_history=person_information['action_history'], memory=person_information['memory'], schedule=person_information['schedule'], observes=..., current_location=..., current_time=...)
 daily_schedule, times = make_design(person_information, design_action)
 
 print(daily_schedule)
