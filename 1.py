0624d6c603326626861d9b979999933646caeba6
larry <larryncent.14616@gmail.com>
2024-11-04 20:34:20 +0800

    modified .gitignore

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ import json
 from planning_prompt import *
 from datetime import datetime
 
-with open("personal_information\lin\information.json", "r", encoding="utf-8") as f:
+with open(".\personal_information\origin_information\p1_information.json", "r", encoding="utf-8") as f:
     person_information = json.load(f)
 
 
@@ -12,13 +12,20 @@ with open("personal_information\lin\information.json", "r", encoding="utf-8") as
 if __name__ == "__main__":
     # 設定大概日程
     broad_plan, _ = make_design(person_information, daily_routine)
-    
-    daily_routine_hourly.format(broad_plan)
+    print("broad_plan:\n", broad_plan)
+
+    #daily_routine_hourly.format(broad_plan)
+    #print("daily_routine_hourly\n", daily_routine_hourly)
+
     # 生成每小時任務的細節
     hourly_plan, _ = make_design(person_information, daily_routine_hourly)
-    
+    print("hourly_plan:\n", hourly_plan)
+    #daily_routine_MIN.format(hourly_plan)
+    #print("daily_routine_MIN:\n", daily_routine_MIN)
+
     # 遞歸細分為 5 到 15 分鐘的小步驟
     detailed_plan, _ = make_design(person_information, daily_routine_MIN)
+    print("detailed_plan:\n", detailed_plan)
     
     # 輸出最終計劃
     print("大致日程:")
